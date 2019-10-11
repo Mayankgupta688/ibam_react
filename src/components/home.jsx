bfe45e36b09efca49d576b0c0ee0d3fb0f195c68
Mayank.Gupta <mayank.gupta@iongroup.com>
2019-10-11 12:52:36 +0530

    Bootstrapping POC for React IBAM

---
+++ src/components/home.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import $ from 'jquery';
+import tablesorter from 'tablesorter';
+import axios from "axios";
+
+export default class HomeComponent extends React.Component {
+
+    constructor() {
+        super();
+        this.state = {
+            employees: []
+        }
+    }
+
+    componentDidMount() {
+        axios.get(`http://localhost:8000/employees`).then(res => {
+            this.setState({
+                employees: res.data
+            });
+            $("#myTable").tablesorter({ sortList: [[0,0], [1,0]] });
+        })
+    }
+
+    render() {
+        var ouputData = this.state.employees.map((user) => {
+            return (
+                <tr>
+                    <td>{user.lastName}</td>
+                    <td>{user.firstName}</td>
+                    <td>{user.email}</td>
+                    <td>{user.Website}</td>
+                </tr>
+            )
+        });
+
+        return (
+            <table id="myTable" className="tablesorter">
+                <thead>
+                    <tr>
+                    <th>Last Name</th>
+                    <th>First Name</th>
+                    <th>Email</th>
+                    <th>Website</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {ouputData}
+                </tbody>
+            </table>
+        )
+    }
+}
