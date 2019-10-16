0f71a7c3290ae2885743e355753b3a1b7f133840
kaurh <harneet.kaur@iongroup.com>
2019-10-16 11:45:44 +0530

    Add Roles

--- src/components/roles/addRoles.jsx
+++ src/components/roles/addRoles.jsx
@@ -2,6 +2,9 @@ import React from "react";
 
 export default class AddRolesComponent extends React.Component {
     render() {
+        if(!this.props.addRole){
+            return null;
+        }
         return (
             <h3>Form To Add Roles</h3>
         )
--- src/components/roles/roles.jsx
+++ src/components/roles/roles.jsx
@@ -1,15 +1,18 @@
 import React from "react";
 import $ from 'jquery';
 import tablesorter from 'tablesorter';
+import '../../styles/roles.css';
 import axios from "axios";
 import { Link } from "react-router-dom";
+import AddRolesComponent from "./addRoles";
 
 export default class RolesComponent extends React.Component {
 
     constructor() {
         super();
         this.state = {
-            roles: []
+            roles: [],
+            isAddRole : false
         }
     }
 
@@ -22,14 +25,26 @@ export default class RolesComponent extends React.Component {
         })
     }
 
+    handleAddRowClick = () => {
+        this.setState(state => ({isAddRole: !this.state.isAddRole}));
+    }
+
     render() {
+        const isAddRole = this.state.isAddRole;
         var ouputData = this.state.roles.map((user) => {
             return (
-                <tr key={user.roleName}>
-                    <td>{user.roleName}</td>
-                    <td>{user.firstName}</td>
-                    <td>{user.email}</td>
-                    <td>{user.Website}</td>
+                <tr key={user.id}>
+                     <td>
+                        <div style={{"white-space": "nowrap", "width": "134px"}}>                      
+                            <a name="edit_" href="/">
+                                <div className="sprite-edit-16px" id="editIcon.img" title="Edit" style={{"display": "inline-flex", "margin-right" : "2px" , "margin-left": "2px", "margin-top": "2px"}}></div>
+                            </a>
+                            <a name="delete_" href="/">
+                                <div className="sprite-delete-16px" id="deleteIcon.img" title="Delete" style={{"display": "inline-flex", "margin-right" : "2px" , "margin-left": "2px", "margin-top": "2px"}}></div>
+                            </a>
+                        </div>
+                    </td>
+                    <td>{user.authority}</td>
                 </tr>
             )
         });
@@ -39,10 +54,8 @@ export default class RolesComponent extends React.Component {
                 {ouputData.length && <table id="myTable" className="tablesorter">
                     <thead>
                         <tr>
+                        <th> </th>
                         <th>Role Name</th>
-                        <th>First Name</th>
-                        <th>Email</th>
-                        <th>Website</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -51,7 +64,10 @@ export default class RolesComponent extends React.Component {
                 </table>}
 
                 <div style={{"width": "100%"}}>
-                    <Link style={{"float": "right", "marginRight": "10px"}} id="viewGroupsUsersMenu" className="addButton" to="/addRoles">Add Roles</Link>
+                    <AddRolesComponent addRole={this.state.isAddRole} />                
+                    <button style={{"float": "right", "marginRight": "10px"}} id="addRole" className="addButton" onClick={this.handleAddRowClick}>
+                       {isAddRole ? 'Save' : 'Add Role'}
+                    </button>
                 </div>
 
             </>
