7ec248173f737746aa2d15f2987bb2ef95b16d36
Mayank.Gupta <mayank.gupta@iongroup.com>
2019-10-22 12:24:03 +0530

    Data for Groups

--- src/components/groups/groups.jsx
+++ src/components/groups/groups.jsx
@@ -8,57 +8,61 @@ export default class GroupsComponent extends React.Component {
     constructor() {
         super();
         this.state = {
-            employees: []
+            groups: []
         }
     }
 
     componentDidMount() {
-        axios.get(`http://localhost:8000/employees`).then(res => {
+        axios.get(`http://localhost:8000/groups`).then(res => {
             this.setState({
-                employees: res.data
+                groups: res.data
             });
             $("#myTable").tablesorter({ sortList: [[0,0], [1,0]] });
         })
     }
 
-    render() {
-        var ouputData = this.state.employees.map((user) => {
-            return (
-                <tr key={user.firstName}>
-                    <td>
-                        <div style={{"white-space": "nowrap", "width": "134px"}}>                      
-                            <i name="edit_roles" onClick={() => this.editRole(user.firstName)}  className="sprite-edit-16px" style={{"display": "inline-flex", "margin-right" : "2px" , "margin-left": "2px", "margin-top": "2px"}}></i>
-                            <i name="delete_roles" onClick={() => this.deleteRole(user.firstName)} className="sprite-delete-16px" style={{"display": "inline-flex", "margin-right" : "2px" , "margin-left": "2px", "margin-top": "2px"}}></i>
-                        </div>
-                    </td>
-                    <td>{user.lastName}</td>
-                    <td>{user.firstName}</td>
-                    <td>{user.email}</td>
-                    <td>{user.Website}</td>
-                </tr>
-            )
-        });
+    createTableData() {
 
+            if(this.state.groups.length) {
+                return (
+                    <table id="myTable" className="tablesorter">
+                        <thead>
+                            <tr>
+                                <th>Actions</th>
+                                <th>User Name</th>
+                                <th>Email Id</th>
+                                <th>Group Name</th>
+                            </tr>
+                        </thead>
+                
+                        <tbody>
+                            {this.state.groups.map((user) => {
+                                return (
+                                    <tr key={user.id}>
+                                        <td>
+                                            <div style={{"white-space": "nowrap", "width": "134px"}}>                      
+                                                <i name="edit_roles" onClick={() => this.editRole(user.id)}  className="sprite-edit-16px" style={{"display": "inline-flex", "margin-right" : "2px" , "margin-left": "2px", "margin-top": "2px"}}></i>
+                                                <i name="delete_roles" onClick={() => this.deleteRole(user.id)} className="sprite-delete-16px" style={{"display": "inline-flex", "margin-right" : "2px" , "margin-left": "2px", "margin-top": "2px"}}></i>
+                                            </div>
+                                        </td>
+                                        <td>{user.name}</td>
+                                        <td>{user.email}</td>
+                                        <td>{user.role}</td>
+                                    </tr>
+                                )
+                            })}
+                        </tbody>
+                    </table>
+                )
+            } else {
+                return null;
+            }
+        }
+
+    render() {
         return (
             <>
-                {ouputData.length && <table id="myTable" className="tablesorter">
-                    <thead>
-                        <tr>
-                            <th>Actions</th>
-                            <th>Last Name</th>
-                            <th>First Name</th>
-                            <th>Email</th>
-                            <th>Website</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {ouputData}
-                    </tbody>
-                </table>}
-
-                <div style={{"width": "100%"}}>
-                    <input style={{"float": "right", "marginRight": "10px"}} type="button" className="addButton" value="Add Groups" />
-                </div>
+                {this.createTableData()}
             </>
         )
     }
